35a589c0d1e8fd5ee43e606ca1b85e55f7839340
rurain7763 <50187123+rurain7763@users.noreply.github.com>
2024-09-20 15:00:35 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240920/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
+++ 240920/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << round((a + b) / (float)(a - b) * 100) / 100;
+    std::cout.precision(2);
+    cout << std::fixed << round((a + b) / (float)(a - b) * 100) / 100;
     return 0;
 }
