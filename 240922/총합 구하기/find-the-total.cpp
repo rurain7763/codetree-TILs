d7772c566bd6e0432c7575b0d2b01a0781ad1b48
rurain7763 <50187123+rurain7763@users.noreply.github.com>
2024-09-22 12:50:48 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/총합 구하기/find-the-total.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int ans = 0;
+    for(int i = a; i <= b; i++) {
+        if(i % 6 == 0 && i % 8) ans += i;
+    }
+    cout << ans;
+    return 0;
+}
