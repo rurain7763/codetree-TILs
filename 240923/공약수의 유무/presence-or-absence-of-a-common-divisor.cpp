db540902ea68ca6943296bf551f2c5562f0f3289
rurain7763 <50187123+rurain7763@users.noreply.github.com>
2024-09-23 11:16:46 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 240923/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -6,14 +6,10 @@ int main() {
     cin >> a >> b;
     bool found = false;
     for(int i = a; i <= b; i++) {
-        int mini = min(i, 1920);
-        for(int j = 2; j <= mini; j++) {
-            if(i % j == 0 && 1920 % j == 0 && 2880 % j == 0) {
-                found = true;
-                break;
-            }
+        if(1920 % i == 0 && 2880 % i == 0) {
+            found = true;
+            break;
         }
-        if(found) break;
     }
     cout << found;
     return 0;
