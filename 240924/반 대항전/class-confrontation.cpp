0b0481150fd73f9ab46dcfba4930906814e1d861
rurain7763 <50187123+rurain7763@users.noreply.github.com>
2024-09-24 12:15:02 +0900

    Title: 반 대항전, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/반 대항전/class-confrontation.cpp
+++ 240924/반 대항전/class-confrontation.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
 
-typedef long long ll;
-
 int main() {
     int n;
     cin >> n;
-    ll arr[4] = {0};
+    long long arr[4] = {0};
     for(int i = 0; i < 4; i++) {
         string str;
         cin >> str;
         for(int j = 1; j <= n; j++) {
-            ll v;
+            int v;
             cin >> v;
             arr[i] += v;
         }
@@ -19,7 +17,7 @@ int main() {
     int maxi = 0;
     for(int i = 0; i < 4; i++) {
         cout << char('A' + i) << " - " << arr[i] << '\n';
-        if(arr[i] > maxi) {
+        if(arr[i] > arr[maxi]) {
             maxi = i;
         }
     }
