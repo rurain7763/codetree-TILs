226550876ee2ed1eb4a8dc24501e2b492eb1e25d
rurain7763 <50187123+rurain7763@users.noreply.github.com>
2024-09-24 11:53:13 +0900

    Title: 반 대항전, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240924/반 대항전/class-confrontation.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[5] = {0};
+    for(int i = 0; i < 4; i++) {
+        string str;
+        cin >> str;
+        for(int j = 1; j <= n; j++) {
+            int v;
+            cin >> v;
+            arr[i] += v;
+        }
+    }
+    int maxi = 0;
+    for(int i = 0; i < 4; i++) {
+        cout << char('A' + i) << " - " << arr[i] << '\n';
+        if(arr[i] > maxi) {
+            maxi = i;
+        }
+    }
+    cout << "Class " << char('A' + maxi) << " is winner!";
+    return 0;
+}
