21f001718292b65d32adfb720c2357ccfdc6db85
rurain7763 <50187123+rurain7763@users.noreply.github.com>
2024-09-27 12:15:15 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240927/미는 횟수/number-of-pushes.cpp
+++ 240927/미는 횟수/number-of-pushes.cpp
@@ -5,13 +5,15 @@ int main() {
     string a, b;
     cin >> a >> b;
     int sz = a.size();
+    int cnt = 0;
     for(int i = sz - 1; i > 0; i--) {
         if(a == b) {
-            cout << i + 1;
+            cout << cnt;
             return 0;
         }
         a = a.substr(0, i);
         b = b.substr(1, i);
+        cnt++;
     }
     cout << -1;
     return 0;
