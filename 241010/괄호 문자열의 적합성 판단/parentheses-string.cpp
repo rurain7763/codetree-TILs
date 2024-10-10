bcda6bc46f878e8c189a0fe830c1d9bdab15ea80
rurain7763 <50187123+rurain7763@users.noreply.github.com>
2024-10-10 11:13:43 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/괄호 문자열의 적합성 판단/parentheses-string.cpp
+++ 241010/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -5,19 +5,17 @@ int main() {
     string str;
     cin >> str;
     int stk = 0;
-    bool ans = true;
     for(char c : str) {
         if(c == '(') {
             stk++;
         } else {
             if(stk == 0) {
-                ans = false;
-                break;
+                cout << "No";
+                return 0;
             }
             stk--;
         }
     }
-    ans = stk == 0;
-    cout << (ans ? "Yes" : "No");
+    cout << (stk == 0 ? "Yes" : "No");
     return 0;
 }
