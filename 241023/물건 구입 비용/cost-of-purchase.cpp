b83ff8aee0dac3722bd38b35752e5ed2228c8fcd
rurain7763 <50187123+rurain7763@users.noreply.github.com>
2024-10-23 11:53:49 +0900

    Title: 물건 구입 비용, Time: 81ms, Memory: 7MB, Status: Wrong Answer - CodeTree

--- 241023/물건 구입 비용/cost-of-purchase.cpp
+++ 241023/물건 구입 비용/cost-of-purchase.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int n, p;
 vector<pair<int, int>> arr;
+int dp[1001][2001];
 int solve(int curCount, int curCost) {
     if(curCount >= n) {
         return curCost;
     }
+
+    if(dp[curCount][curCost] != 987654321) return dp[curCount][curCost];
 
     int ret = 987654321;
     for(int i = 0; i < p; i++) {
@@ -15,12 +18,17 @@ int solve(int curCount, int curCost) {
         ret = min(ret, solve(curCount + pa.first, curCost + pa.second));
     }
 
-    return ret;
+    return dp[curCount][curCost] = ret;
 }
 
 int main() {
     cin >> n >> p;
     arr.resize(p);
+    for(int i = 0; i < 1001; i++) {
+        for(int j = 0; j < 2001; j++) {
+            dp[i][j] = 987654321;
+        }
+    }
     for(int i = 0; i < p; i++) {
         cin >> arr[i].first >> arr[i].second;
     }
